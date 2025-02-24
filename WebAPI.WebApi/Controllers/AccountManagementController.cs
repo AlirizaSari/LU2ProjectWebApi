b3d70ee3ec2edaad4039cc3d33261466913f3fd1
AlirizaSari <AlisSari55@hotmail.com>
2025-02-24 21:47:16 +0100

    Update AccountManagementController.cs

--- WebAPI.WebApi/Controllers/AccountManagementController.cs
+++ WebAPI.WebApi/Controllers/AccountManagementController.cs
@@ -21,44 +21,6 @@ public class AccountManagementController : ControllerBase
         this.roleManager = roleManager;
     }
 
-    [HttpPost(Name = "CreateUser")]
-    [Authorize(Policy = "Admin")]
-    public async Task<ActionResult<User>> CreateUser(User user)
-    {
-        var identityUser = new IdentityUser
-        {
-            UserName = user.UserName,
-            Email = user.Email
-        };
-        var result = await userManager.CreateAsync(identityUser, user.PasswordHash);
-        if (result.Succeeded)
-        {
-            return Ok(user);
-        }
-        return BadRequest(result.Errors);
-    }
-
-    [HttpDelete("{id}", Name = "DeleteUser")]
-    [Authorize(Policy = "Admin")]
-    public async Task<ActionResult<User>> DeleteUser(string id)
-    {
-        if (id is null)
-        {
-            return BadRequest("Id is required");
-        }
-        var user = await userManager.FindByIdAsync(id);
-        if (user == null)
-        {
-            return NotFound();
-        }
-        var result = await userManager.DeleteAsync(user);
-        if (result.Succeeded)
-        {
-            return Ok();
-        }
-        return BadRequest(result.Errors);
-    }
-
     [HttpPost("{id}/{claim}/{value?}", Name = "AddClaimToUser")]
     public async Task<ActionResult<User>> AddClaimToUser(string id, string claim, string value = null!)
     {
